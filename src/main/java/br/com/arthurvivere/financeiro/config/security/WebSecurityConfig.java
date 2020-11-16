4fe9e26f7bf5fb7ecc4a55d7cbef6a1233f1fa06
Arthur Azevedo <arthur.stevam.costa@ccc.ufcg.edu.br>
2020-11-16 14:50:47 -0300

    Swagger documentation add

--- src/main/java/br/com/arthurvivere/financeiro/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/arthurvivere/financeiro/config/security/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -46,5 +47,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioServices), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs",
+                "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 }
